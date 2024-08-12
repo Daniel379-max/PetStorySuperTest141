d87f5e3244452e0fa925c848274da2f26701513d
Daniel Romagnoli <danielromagnoli12@gmail.com>
2024-08-12 17:41:53 -0300

    teste comentario

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -4,9 +4,10 @@ const petId = 473357
 
 describe('API PetStore Swagger - Entidade Pet', () => {
 
-    const request = supertest('https://petstore.swagger.io/v2')
+        const request = supertest('https://petstore.swagger.io/v2')
 
-    it('POST Pet', () => {
+        it('POST Pet aaaaa', () => {
 
+        })
     })
-})
+    // teste
