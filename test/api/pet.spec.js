f7e432155dbc25c959be9e28bdc7d7546bba9db6
Daniel Romagnoli <danielromagnoli12@gmail.com>
2024-08-12 17:37:46 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,12 @@
+const supertest = require('supertest')
+
+const petId = 473357
+
+describe('API PetStore Swagger - Entidade Pet', () => {
+
+    const request = supertest('https://petstore.swagger.io/v2')
+
+    it('POST Pet', () => {
+
+    })
+})
