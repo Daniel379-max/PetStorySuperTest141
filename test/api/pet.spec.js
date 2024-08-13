8ce28278b1c901d15e18b628446dead0af93cefd
Daniel Romagnoli <danielromagnoli12@gmail.com>
2024-08-13 19:37:48 -0300

    commit antigo

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -7,7 +7,16 @@ describe('API PetStore Swagger - Entidade Pet', () => {
         const request = supertest('https://petstore.swagger.io/v2')
 
         it('POST Pet aaaaa', () => {
-
+            const pet = require('../../vendors/json/pet.json')
+            return request
+                .post('/pet')
+                .send(pet)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(petId)
+                    expect(res.body.name).toBe('doguinho')
+                    expect(res.body.category.id).toBe(1)
+                })
         })
     })
     // teste
