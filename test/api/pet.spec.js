d5bf62128957708b472959d756811369f787337c
Daniel Romagnoli <danielromagnoli12@gmail.com>
2024-08-16 20:30:37 -0300

    versoes data driven

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -77,10 +77,60 @@ describe('API PetStore Swagger - Entidade Pet', () => {
                 expect(res.body.message).toBe(`${petId}`)
             })
     })
-
     massaInicial.array.forEach(({ nomePet, idPet, nomeCategoria, idCategoria }) => {
-        it('POST Pet Data Driven ForEach- ${nomePet}', () => {
+        it(`POST Pet Data Driven ForEach- ${nomePet}`, () => {
+            const pet = require('../../vendors/json/pet.json')
 
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+
+            return request
+                .post('/pet')
+                .send(pet)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(idPet)
+                    expect(res.body.name).toBe(nomePet)
+                    expect(res.body.category.name).toBe(nomeCategoria)
+                    expect(res.body.tags[0].name).toBe("vacinned")
+                })
+        })
+        it(`GET Pet Data Driven ForEach- ${nomePet}`, async() => {
+            return request
+                .get(`/pet/${idPet}`)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.id).toBe(idPet)
+                    expect(res.body.status).toBe("available")
+                })
+        })
+
+        it(`PUT Pet Data Driven ForEach- ${nomePet}`, () => {
+            const pet = require('../../vendors/json/petput.json')
+
+            pet.id = idPet
+            pet.name = nomePet
+            pet.category.id = idCategoria
+            pet.category.name = nomeCategoria
+
+            return request
+                .put('/pet')
+                .send(pet)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.status).toBe('sold')
+                })
+        })
+
+        it(`DELETE Pet Data Driven ForEach- ${nomePet}`, () => {
+            return request
+                .delete(`/pet/${idPet}`)
+                .then((res) => {
+                    expect(res.statusCode).toBe(200)
+                    expect(res.body.message).toBe(`${idPet}`)
+                })
         })
     })
 })
